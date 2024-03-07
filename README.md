3df597cbbe90fcd2e94cf5f13c24a878bb81c8ac
javid4962 <shaikmasthanjavidvali@gmail.com>
2024-03-07 12:55:20 +0530

    Todo app updated

---
+++ README.md
@@ -0,0 +1,17 @@
+# To-Do-App
+## Skills used:
+- HTML
+- CSS 
+- JavaScript
+- Editor: VSCode
+- Deployment Link: <a href="https://javid4962-to-do-app.netlify.app" target="blank">https://javid4962-to-do-app.netlify.app</a>
+
+## Images:
+### Old Project - 2023
+![alt](images/image.png)
+![alt](images/image1.png)
+![alt](images/image2.png)
+
+### Changes made on - 2024
+![alt](images/image3.png)
+![alt](images/image4.png)
--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container")
 
+// time = time.getDate()+"/"+month+"/"+time.getFullYear()+" - "+time.getHours()+":"+time.getMinutes()
+// console.log(time)
 function addTask() {
-    if (inputBox.value === '') {
+    if (inputBox.value === ''||null) {
         alert('Enter some task!!!')
     }
     else {
+        var time = new Date()
+        var month = time.getMonth()+1
+        var time = time.getDate()+"/"+month+"/"+time.getFullYear()+" - "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()
         let li = document.createElement('li');
-        li.innerHTML = inputBox.value;
+        li.innerHTML = inputBox.value+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+time;
         listContainer.appendChild(li);
         let span = document.createElement('span');;
         span.innerHTML='\u00d7';
